0a2b6e0698397951d338f945d4fae37ce1859777
angel_ivanov <angelspsv@gmail.com>
2025-01-13 17:16:24 +0100

    exercici 1 examen

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI
+from connection import *
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# metode per veure si funciona tot
+@app.get("/")
+def salutacio():
+    return "Hola"
+
+
+# Exercici 1
+class Formulari(BaseModel):
+    nombre: str
+    apellido: str
+    email: str
+    descripcion: str | None = None
+    curso: int
+    anio: int
+    direccion: str
+    codi_postal: int | None = None
+    password: str
